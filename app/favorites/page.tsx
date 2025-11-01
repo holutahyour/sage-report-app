7bc6d1a2b1ac2385e7436b1c8670f2edf5884246
Victor Onibudo <tonibudo@lagetronix.com>
2025-11-01 04:01:44 +0100

    redirect home page to favorite

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -35,7 +35,7 @@ export default function FavoritesPage() {
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <div className="flex flex-col sm:gap-4 sm:py-4">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
           <h1 className="text-2xl font-bold">Favorites Dashboard</h1>
           <Button variant="outline" size="sm" className="ml-auto">
